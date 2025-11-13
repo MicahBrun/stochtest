20e1edd2f90c3c2c4ce169b7ab34d5a88aeb96b4
Micah Brown <micahgbrown@gmail.com>
2025-11-13 17:05:34 +0000

    Updated Statistical Assertion Changed toml

--- src/stochtest/core.py
+++ src/stochtest/core.py
@@ -3,20 +3,47 @@
 # SPDX-License-Identifier: MIT
 from scipy import stats as ss
 from typing import TypeVar, Generic, Iterable
+import numpy as np
 
 T = TypeVar('T')
 class StatisticalAssertion(Generic[T]):
+    _accepted_alternatives = ["less", "greater", "two-sided"]
     def __init__(self, samples: Iterable[T]):
         self._samples = samples
     
-    def has_acceptance_rate(self, target_rate, alpha=0.05):
-        k = sum(self._samples)
-        n = len(self._samples)
+    def has_acceptance_rate(self, target_rate, alpha=0.05, alternative='less'):
+        self._validate_single_assertion()
+        self._validate_alternative(alternative)
 
-        p_value = ss.binomtest(k, n, target_rate, alternative='less')
-        if (p_value < alpha):
-            observed_rate = k / n
+        arr = np.asarray(self._samples, dtype=bool)
+        self._samples = None
+
+        k = sum(arr)
+        n = len(arr)
+
+        result = ss.binomtest(k, n, target_rate, alternative=alternative)
+        if (result.pvalue < alpha):
             raise AssertionError(
-                f"Observed rate ({observed_rate:.4f}) is significantly less than"
-                f"target ({target_rate:.4f}) (p={p_value:.4e} < alpha={alpha})."
+                f"Observed rate ({result.statistic:.4f}) is significantly {StatisticalAssertion._alternative_to_description(alternative)} than"
+                f"target ({target_rate:.4f}) (p={result.pvalue:.4e} < alpha={alpha})."
             )
+    
+    def _validate_single_assertion(self):
+        if self._samples == None:
+            raise RuntimeError("Multiple assertion methods called on the same StatisticalAssertion instance.")
+        
+    def _validate_alternative(self, alternative: str):
+        if alternative not in self._accepted_alternatives:
+            raise ValueError(f"alternative ({alternative}) is not a valid argument.")
+        
+    @staticmethod
+    def _alternative_to_description(alternative):
+        match alternative:
+            case "less":
+                return "less than"
+            case "greater":
+                return "greater than"
+            case "two-sided":
+                return "different from"
+            case _:
+                return ""
