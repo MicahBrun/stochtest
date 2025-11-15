e47b0a54f6de4b3e163866d800f5ad119c8ff17b
Micah Brown <micahgbrown@gmail.com>
2025-11-15 01:29:01 +0000

    Changed publish to publish via github

--- src/internal/_core.py
+++ src/internal/_core.py
@@ -6,19 +6,33 @@ from typing import Iterable
 import numpy as np
 
 class StatisticalAssertion():
-    _acceptance_conditions = ["less", "greater"]
     def __init__(self, *samples: Iterable):
         self._samples = samples
-    
-    def has_acceptance_rate(self, target_rate, alpha=0.05, acceptance_condition='greater'):
+
+    def has_acceptance_rate_less_than(self, target_rate, alpha=0.05):
         self._validate_single_assertion()
-        self._validate_alternative(acceptance_condition)
 
-        arr = np.asarray(self._samples[0], dtype=bool)
-        self._samples = None
+        samples = self._get_samples_singleton()
+        arr = np.asarray(samples, dtype=bool)
 
-        k = np.sum(arr)
-        n = arr.size
+        self._enforce_single_assertion()
+
+        return self._assert_acceptance_rate_check(arr, target_rate, alpha, "less")
+    def has_acceptance_rate_greater_than(self, target_rate, alpha=0.05):
+        self._validate_single_assertion()
+
+        samples = self._get_samples_singleton()
+        arr = np.asarray(samples, dtype=bool)
+
+        self._enforce_single_assertion()
+
+        return self._assert_acceptance_rate_check(arr, target_rate, alpha, "greater")
+    
+    _acceptance_conditions = ["less", "greater"]
+    def _assert_acceptance_rate_check(self, samples: np.ndarray[np.bool], target_rate: float, alpha: float, acceptance_condition='greater'):
+
+        k = np.sum(samples)
+        n = samples.size
 
         result = ss.binomtest(k, n, target_rate, alternative=StatisticalAssertion._acceptance_condition_to_alternative(acceptance_condition))
         if (result.pvalue > alpha):
@@ -31,6 +45,10 @@ class StatisticalAssertion():
     def _validate_single_assertion(self):
         if self._samples is None:
             raise RuntimeError("Multiple assertion methods called on the same StatisticalAssertion instance.")
+    
+    def _enforce_single_assertion(self):
+        self._validate_single_assertion()
+        self._samples = None
         
     def _validate_alternative(self, alternative: str):
         if alternative not in self._acceptance_conditions:
@@ -48,5 +66,10 @@ class StatisticalAssertion():
     
     @staticmethod
     def _acceptance_condition_to_alternative(acceptance_condition: str) -> str:
-        #conditions are identical to alternative
+        #acceptance_conditions are a subset of alternatives in scipy
         return acceptance_condition
+    
+    def _get_samples_singleton(self) -> Iterable:
+        if (len(self._samples) != 1):
+            raise ValueError(f"{len(self._samples)} sample collections provided, exactly 1 was expected.")
+        return self._samples[0]
--- src/stochtest/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present Micah Brown
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.2"
