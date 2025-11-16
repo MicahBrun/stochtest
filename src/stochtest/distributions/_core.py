0a7fe7d884c75396346c1f0a10780d1a1e96a21d
Micah Brown <micahgbrown@gmail.com>
2025-11-16 18:06:13 +0000

    Added batching to has_distribution

--- src/stochtest/distributions/_core.py
+++ src/stochtest/distributions/_core.py
@@ -9,16 +9,25 @@ class _DistributionsStatisticalAssertion:
     def __init__(self, samples: Iterable):
         self._samples = samples
 
-    def has_distribution(self, reference_samples: Iterable, margin = 0.01, confidence = 0.95, n_bootstraps = 1000, random_state=None):
+    def has_distribution(self, reference_samples: Iterable, margin = 0.02, confidence = 0.95, n_bootstraps = 500, random_state=None):
         self._validate_single_assertion()
-        
-        n=len(self._samples)
+        MAX_INDIVIDUAL_SAMPLES_PER_BATCH = 1_000_000
 
         generator = np.random.default_rng(random_state)
-        sample_bootstraps = generator.choice(self._samples, size=(n_bootstraps, n), replace=True)
 
-        ks_output = ss.ks_2samp(sample_bootstraps, reference_samples, axis=1)
-        ks_distances = ks_output.statistic
+        n=len(self._samples)
+        max_batch_size = max(1, MAX_INDIVIDUAL_SAMPLES_PER_BATCH//n)
+
+        ks_distances = np.array(
+            [d
+                for batch_size in _DistributionsStatisticalAssertion.batch_sizes(
+                    n_bootstraps, 
+                    max_batch_size)
+                for d in _DistributionsStatisticalAssertion._apply_ks_2samp_to_batch(
+                    self._samples, 
+                    reference_samples, 
+                    batch_size, 
+                    generator)])
 
         successes = ks_distances <= margin
         success_rate = sum(successes)/n_bootstraps
@@ -29,6 +38,13 @@ class _DistributionsStatisticalAssertion:
 
         if (success_rate < confidence):
             raise AssertionError(f"Confidence ({success_rate}) less than ({confidence}). Distance={base_ks_distance}")
+    
+    @staticmethod
+    def _apply_ks_2samp_to_batch(samples, reference_samples, n_bootstraps_in_batch, generator):
+        n=len(samples)
+        sample_bootstraps = generator.choice(samples, size=(n_bootstraps_in_batch, n), replace=True)
+        ks_output = ss.ks_2samp(sample_bootstraps, reference_samples, axis=1)
+        return ks_output.statistic
 
 
     def _validate_single_assertion(self):
@@ -38,3 +54,12 @@ class _DistributionsStatisticalAssertion:
     def _enforce_single_assertion(self):
         self._validate_single_assertion()
         self._samples = None
+    
+    @staticmethod
+    def batch_sizes(n, d):
+        full_batches = n // d
+        remainder = n % d
+        for _ in range(full_batches):
+            yield d
+        if remainder:
+            yield remainder
